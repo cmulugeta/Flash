1cf51ed5b44b5575fa6ffe839928001375fdf300
mulugeta c <cmulugeta.up@gmail.com>
2018-01-21 15:05:50 -0600

    update interceptor, update home screen

--- app/src/main/java/com/cmulugeta/mediaplayer/ui/home/favorite/LovedFragment.kt
+++ app/src/main/java/com/cmulugeta/mediaplayer/ui/home/favorite/LovedFragment.kt
@@ -4,25 +4,28 @@ import com.cmulugeta.mediaplayer.App
 import com.cmulugeta.mediaplayer.R
 import com.cmulugeta.mediaplayer.di.component.DaggerViewComponent
 import com.cmulugeta.mediaplayer.di.module.PresenterModule
+import com.cmulugeta.mediaplayer.di.qualifier.Loved
 import com.cmulugeta.mediaplayer.ui.home.HomeContract.Presenter
 import com.cmulugeta.mediaplayer.ui.home.HomeFragment
 import javax.inject.Inject
-import com.cmulugeta.mediaplayer.di.qualifier.Loved
 
 class LovedFragment : HomeFragment() {
-    @Inject
-    override fun attach(@Loved presenter: Presenter) {
-        this.presenter=presenter
-        presenter.attach(this)
+  override var presenter: Presenter? = null
+    @Inject set(@Loved value) {
+      field = value
+      field?.attach(this)
     }
 
-    override fun inject() {
-        DaggerViewComponent.builder()
-                .applicationComponent(App.component)
-                .presenterModule(PresenterModule())
-                .build().inject(this)
-    }
+  override fun inject() {
+    DaggerViewComponent.builder()
+        .applicationComponent(App.component)
+        .presenterModule(PresenterModule())
+        .build().inject(this)
+  }
 
-    override fun emptyMessage(): Int =R.string.empty_liked
-    override fun alertMessage():String = getString(R.string.loved_alert)
+  override fun emptyMessage(): Int = R.string.empty_liked
+  override fun alertMessage(): String = getString(R.string.loved_alert)
+
+  override fun attach(presenter: Presenter) {
+  }
 }
--- app/src/main/java/com/cmulugeta/mediaplayer/ui/home/history/HistoryFragment.kt
+++ app/src/main/java/com/cmulugeta/mediaplayer/ui/home/history/HistoryFragment.kt
@@ -4,25 +4,28 @@ import com.cmulugeta.mediaplayer.App
 import com.cmulugeta.mediaplayer.R
 import com.cmulugeta.mediaplayer.di.component.DaggerViewComponent
 import com.cmulugeta.mediaplayer.di.module.PresenterModule
+import com.cmulugeta.mediaplayer.di.qualifier.History
 import com.cmulugeta.mediaplayer.ui.home.HomeContract.Presenter
 import com.cmulugeta.mediaplayer.ui.home.HomeFragment
-import com.cmulugeta.mediaplayer.di.qualifier.History
 import javax.inject.Inject
 
-class HistoryFragment : HomeFragment(){
-    @Inject
-    override fun attach(@History presenter: Presenter) {
-        this.presenter=presenter
-        presenter.attach(this)
+class HistoryFragment : HomeFragment() {
+  override var presenter: Presenter? = null
+    @Inject set(@History value) {
+      field = value
+      field?.attach(this)
     }
 
-    override fun inject() {
-        DaggerViewComponent.builder()
-                .applicationComponent(App.component)
-                .presenterModule(PresenterModule())
-                .build().inject(this)
-    }
+  override fun inject() {
+    DaggerViewComponent.builder()
+        .applicationComponent(App.component)
+        .presenterModule(PresenterModule())
+        .build().inject(this)
+  }
 
-    override fun emptyMessage(): Int =R.string.empty_history
-    override fun alertMessage():String = getString(R.string.history_alert)
+  override fun attach(presenter: Presenter) {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+  override fun emptyMessage(): Int = R.string.empty_history
+  override fun alertMessage(): String = getString(R.string.history_alert)
 }
